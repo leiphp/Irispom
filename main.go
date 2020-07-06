681e84834296df34d8cbab79ab808790e039b089
leiwen <leiwen@jmfen.cn>
2020-07-06 20:20:49 +0800

    feat：add views

--- main.go
+++ main.go
@@ -6,8 +6,22 @@ func main() {
 	app := iris.Default()
 	app.Use(myMiddleware)
 
+	app.RegisterView(iris.HTML("./views", ".html"))
+
 	app.Handle("GET", "/ping", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"message": "pong"})
+	})
+
+	app.Get("/", func(ctx iris.Context) {
+		// Bind: {{.message}} with "Hello world!"
+		ctx.ViewData("message", "Hello world!")
+		// Render template file: ./views/hello.html
+		ctx.View("hello.html")
+	})
+
+	app.Get("/user/{id:uint64}", func(ctx iris.Context) {
+		userID, _ := ctx.Params().GetUint64("id")
+		ctx.Writef("User ID: %d", userID)
 	})
 
 	// Listens and serves incoming http requests
